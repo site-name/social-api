c4f6920826336b9602e734aab654fe3820d6f2b9
leminhson2398 <leminhson2398@outlook.com>
2021-05-20 10:19:47 +0700

    commit

--- model/wishlist/wishlist.go
+++ model/wishlist/wishlist.go
@@ -1,8 +1,56 @@
 package wishlist
 
-type Wishlish struct {
+import (
+	"io"
+
+	"github.com/sitename/sitename/model"
+)
+
+type Wishlist struct {
 	Id       string  `json:"id"`
-	Token    string  `json:"token"`
+	Token    string  `json:"token"` // uuid, unique
 	UserID   *string `json:"user_id"`
 	CreateAt int64   `json:"create_at"`
+}
+
+func (w *Wishlist) IsValid() *model.AppError {
+	outer := model.CreateAppErrorForModel(
+		"model.wishlist.is_valid.%s.app_error",
+		"wishlist_id=",
+		"Wishlist.IsValid",
+	)
+	if !model.IsValidId(w.Id) {
+		return outer("id", nil)
+	}
+	if !model.IsValidId(w.Token) {
+		return outer("token", &w.Id)
+	}
+	if w.UserID != nil && !model.IsValidId(*w.UserID) {
+		return outer("user_id", &w.Id)
+	}
+	if w.CreateAt == 0 {
+		return outer("create_at", &w.Id)
+	}
+
+	return nil
+}
+
+func (w *Wishlist) ToJson() string {
+	return model.ModelToJson(w)
+}
+
+func WishlistFromJson(data io.Reader) *Wishlist {
+	var w Wishlist
+	model.ModelFromJson(&w, data)
+	return &w
+}
+
+func (w *Wishlist) PreSave() {
+	if w.Id == "" {
+		w.Id = model.NewId()
+	}
+	if w.Token == "" {
+		w.Token = model.NewId()
+	}
+	w.CreateAt = model.GetMillis()
 }
--- model/wishlist/wishlist_item.go
+++ model/wishlist/wishlist_item.go
@@ -1,15 +1,52 @@
 package wishlist
 
 import (
+	"io"
+
+	"github.com/sitename/sitename/model"
 	"github.com/sitename/sitename/model/product_and_discount"
 )
 
-type WishlishItem struct {
+type WishlistItem struct {
 	Id         string                                 `json:"id"`
-	WishListID string                                 `json:"wishlist_id"`
+	WishlistID string                                 `json:"wishlist_id"`
 	ProductID  string                                 `json:"product_id"`
 	Variants   []*product_and_discount.ProductVariant `json:"variants" db:"-"`
 	CreateAt   int64                                  `json:"create_at"`
-	Wishlish   *Wishlish                              `json:"wishlist" db:"-"`
-	Product    *product_and_discount.Product          `json:"product" db:"-"`
+}
+
+func (w *WishlistItem) IsValid() *model.AppError {
+	outer := model.CreateAppErrorForModel(
+		"model.wishlist_item.is_valid.%s.app_error",
+		"wishlist_item_id=",
+		"WishlistItem.IsValid",
+	)
+	if !model.IsValidId(w.Id) {
+		return outer("id", nil)
+	}
+	if !model.IsValidId(w.WishlistID) {
+		return outer("wishlist_id", &w.Id)
+	}
+	if !model.IsValidId(w.ProductID) {
+		return outer("product_id", &w.Id)
+	}
+
+	return nil
+}
+
+func (w *WishlistItem) PreSave() {
+	if w.Id == "" {
+		w.Id = model.NewId()
+	}
+	w.CreateAt = model.GetMillis()
+}
+
+func (w *WishlistItem) ToJson() string {
+	return model.ModelToJson(w)
+}
+
+func WishlistItemFromJson(data io.Reader) *WishlistItem {
+	var w WishlistItem
+	model.ModelFromJson(&w, data)
+	return &w
 }
