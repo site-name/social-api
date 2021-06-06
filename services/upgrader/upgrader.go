7031237a5ed4b4a33c947447b755edd10dc1e6e8
leminhson2398 <leminhson2398@outlook.com>
2021-06-06 11:01:44 +0700

    commit

---
+++ services/upgrader/upgrader.go
@@ -0,0 +1,13 @@
+package upgrader
+
+// func CanIUpgradeToE0() error {
+// 	return &InvalidArch{}
+// }
+
+// func UpgradeToE0() error {
+// 	return &InvalidArch{}
+// }
+
+// func UpgradeToE0Status() (int64, error) {
+// 	return 0, &InvalidArch{}
+// }
