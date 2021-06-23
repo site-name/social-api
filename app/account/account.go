8e310c566b25d223c08061960235943fb5ff5fd1
leminhson2398 <leminhson2398@outlook.com>
2021-06-23 10:29:55 +0700

    commit

--- app/account/account.go
+++ app/account/account.go
@@ -1,14 +1,8 @@
 package account
 
 import (
-	"errors"
-	"net/http"
-
 	"github.com/sitename/sitename/app"
 	"github.com/sitename/sitename/app/sub_app_iface"
-	"github.com/sitename/sitename/model"
-	"github.com/sitename/sitename/model/account"
-	"github.com/sitename/sitename/store"
 )
 
 type AppAccount struct {
@@ -19,18 +13,4 @@ func init() {
 	app.RegisterAccountApp(func(a app.AppIface) sub_app_iface.AccountApp {
 		return &AppAccount{a}
 	})
-}
-
-func (a *AppAccount) GetAddressById(id string) (*account.Address, *model.AppError) {
-	address, err := a.Srv().Store.Address().Get(id)
-	if err != nil {
-		var nfErr *store.ErrNotFound
-		var statusCode int = http.StatusInternalServerError
-		if errors.As(err, &nfErr) {
-			statusCode = http.StatusNotFound
-		}
-		return nil, model.NewAppError("GetAddressById", "app.address.address_by_id.app_error", nil, err.Error(), statusCode)
-	}
-
-	return address, nil
 }
--- app/account/address.go
+++ app/account/address.go
@@ -1,1 +1,24 @@
 package account
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/sitename/sitename/model"
+	"github.com/sitename/sitename/model/account"
+	"github.com/sitename/sitename/store"
+)
+
+func (a *AppAccount) GetAddressById(id string) (*account.Address, *model.AppError) {
+	address, err := a.Srv().Store.Address().Get(id)
+	if err != nil {
+		var nfErr *store.ErrNotFound
+		var statusCode int = http.StatusInternalServerError
+		if errors.As(err, &nfErr) {
+			statusCode = http.StatusNotFound
+		}
+		return nil, model.NewAppError("GetAddressById", "app.address.address_by_id.app_error", nil, err.Error(), statusCode)
+	}
+
+	return address, nil
+}
