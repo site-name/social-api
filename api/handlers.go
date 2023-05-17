052920e821d8b8ebecb0e6f4836d87dff37a4f36
iamleson98 <leminhson2398@outlook.com>
2023-05-17 07:47:11 +0700

    commit

--- api/handlers.go
+++ api/handlers.go
@@ -141,7 +141,7 @@ func (api *API) APIHandler(h handlerFunc) http.Handler {
 // 		RequireMfa:      false,
 // 		IsStatic:        false,
 // 		IsLocal:         false,
-// 		DisableWhenBusy: true,
+// 		DisableWhenBusy: "Hello",
 // 	}
 // 	if *api.srv.Config().ServiceSettings.WebserverMode == "gzip" {
 // 		return gziphandler.GzipHandler(handler)
